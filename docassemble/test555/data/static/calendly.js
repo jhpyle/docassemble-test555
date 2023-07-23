cd22d128e1644b26834fe540896beec999d46d29
Jonathan Pyle <jhpyle@gmail.com>
2023-07-23 15:15:02 -0400

    modifying readme

---
+++ docassemble/test555/data/static/calendly.js
@@ -0,0 +1,3 @@
+$(document).on('daPageLoad', function(){
+  Calendly.initBadgeWidget({ url: 'https://calendly.com/rothfirm/30min', text: 'Schedule Consultation', color: '#f58220', textColor: '#ffffff', branding: false });
+});
